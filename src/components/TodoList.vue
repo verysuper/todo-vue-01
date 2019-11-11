3c3c8e30d532cee901cceb4d8b4d9075fc5f2c54
熊大 <brown@google.com>
2019-11-11 13:01:59 +0800

    v-model="newTodo"

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    todo list goes here
+    <input type="text"
+           class="todo-input"
+           placeholder="what needs to be done"
+           v-model="newTodo"
+    >
+    {{ newTodo }}
   </div>
 </template>
 
 <script>
     export default {
-        name: "todo-list"
+        name: "todo-list",
+        data(){
+            return{
+                newTodo:'',
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss">
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
 
+    &:focus {
+      outline: 0;
+    }
+  }
 </style>
