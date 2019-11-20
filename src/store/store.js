f6fa7eaffa67f00ad8e71ee81b4f7b21838a8b06
熊大 <brown@google.com>
2019-11-20 16:52:27 +0800

    4.actions add 非同步

--- src/store/store.js
+++ src/store/store.js
@@ -67,22 +67,34 @@ export const store = new Vuex.Store({
   },
   actions:{
     addTodo (context, todo) {
-      context.commit('addTodo',todo);
+      setTimeout(() => {
+        context.commit('addTodo', todo)
+      }, 1000)
     },
     updateTodo(context,todo) {
-      context.commit('updateTodo',todo);
+      setTimeout(() => {
+        context.commit('updateTodo',todo);
+      }, 1000)
     },
     deleteTodo(context,id) {
-      context.commit('deleteTodo',id);
+      setTimeout(() => {
+        context.commit('deleteTodo',id);
+      }, 1000)
     },
     allChecked(context,checked) {
-      context.commit('allChecked',checked);
+      setTimeout(() => {
+        context.commit('allChecked',checked);
+      }, 1000)
     },
     updateFilter(context, filter) {
-      context.commit('updateFilter',filter);
+      setTimeout(() => {
+        context.commit('updateFilter',filter);
+      }, 1000)
     },
     clearCompleted(context) {
-      context.commit('clearCompleted');
+      setTimeout(() => {
+        context.commit('clearCompleted');
+      }, 1000)
     }
   }
 });
